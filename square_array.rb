5cfaabfb7762240aab11bb4e592a77acd81f391b
AbysmalPsycho <rischthefisch@gmail.com>
2020-10-09 08:52:05 -0700

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
+require 'pry'
+
+def square_array(numbers)
+  result_array = []
+
+  numbers.each do |result|
+       result_array << result**2
+       
+  end
+  result_array
 end
+
+# def square_array(numbers)
+   
+
+#  result_array = numbers.collect{|result| result**2}
+      
+# end
